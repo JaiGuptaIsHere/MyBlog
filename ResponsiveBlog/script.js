b0223eab4b7416cbda9b6310a06af8bafdb5e590
Jai Gupta <105773004+JaiGuptaIsHere@users.noreply.github.com>
2022-10-11 00:42:58 +0530

    Add files via upload

---
+++ ResponsiveBlog/script.js
@@ -0,0 +1,9 @@
+const search = document.querySelector(".mine");
+
+
+    search.addEventListener("click", function(){
+        alert("The backend and database set-up is still in progress, please co-operate for a while.");
+    })
+
+
+   
